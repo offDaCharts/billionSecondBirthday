f63f3527cf090a6b1abad1dd252276c9caa0fd2d
Quin Thames <robqthames@gmail.com>
2014-08-10 22:09:53 -0700

    stuff happens second page load and go button causes scroll

--- js/bsb.js
+++ js/bsb.js
@@ -1,20 +1,30 @@
 $(function() {
-	$("form").submit(
-		var birthday = moment($("input.birthday").val()),
-			secondMilestones = {
-				'billion': 1e9,
-				'million': 1e6,
-				'thousand': 1e3
-			},
-			millisecondMultiplier = 1e3,
-			$milestone,
-			milestoneMoment;
+	$('#fullpage').fullpage({
+		afterLoad: function(anchorLink, index) {
+			var tableDisplayPageNum = 2;
+			if (index === tableDisplayPageNum) {
+				var birthday = moment($("input.birthday").val()),
+					secondMilestones = {
+						'billion': 1e9,
+						'million': 1e6,
+						'thousand': 1e3
+					},
+					millisecondMultiplier = 1e3,
+					$milestone,
+					milestoneMoment;
 
-		for(milestone in secondMilestones) {
-			$milestone = $("#" + milestone);
-			milestoneMoment = moment(birthday + secondMilestones[milestone]*millisecondMultiplier);
-			$milestone.find(".livestamp").livestamp(milestoneMoment);
-			$milestone.find(".date").text(milestoneMoment.toLocaleString())
+
+				for(milestone in secondMilestones) {
+					$milestone = $("#" + milestone);
+					milestoneMoment = moment(birthday + secondMilestones[milestone]*millisecondMultiplier);
+					$milestone.find(".livestamp").livestamp(milestoneMoment);
+					$milestone.find(".date").text(milestoneMoment.toLocaleString())
+				}
+			}
 		}
-	);
+	});
+
+	$("#goButton").click(function() {
+		$("#fullpage").fullpage.moveSectionDown();
+	});
 });
