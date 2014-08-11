219b12575f6283e39972210f0eb66621ad83ef92
offDaCharts <robqthames@gmail.com>
2014-08-10 23:48:49 -0700

    milestone flipclocks

--- js/bsb.js
+++ js/bsb.js
@@ -3,7 +3,10 @@ $(function() {
 
 	$fullpage.fullpage({
 		afterLoad: function(anchorLink, index) {
-			var tableDisplayPageNum = 2;
+			var tableDisplayPageNum = 2,
+				getSecondsFromNow = function(thisMoment) {
+					return Math.round(moment().diff(bdaytime)/millisecondMultiplier);
+				}
 
 			if (index === tableDisplayPageNum) {
 				var bdaytime = moment($("input[name=bdaytime]").val()),
@@ -14,19 +17,29 @@ $(function() {
 					},
 					millisecondMultiplier = 1e3,
 					$milestone,
-					milestoneMoment;
+					milestoneMoment,
+					secondsFromNow,
+					flipclockParameters;
 
 				$('#current.flipclock')
 					.empty()
-					.FlipClock(Math.round(moment().diff(bdaytime)/millisecondMultiplier), {
+					.FlipClock(getSecondsFromNow(bdaytime)), {
 						clockFace: 'Counter',
 						autoStart: true
 					});
 
+
 				for(milestone in secondMilestones) {
 					$milestone = $("#" + milestone);
 					milestoneMoment = moment(bdaytime + secondMilestones[milestone]*millisecondMultiplier);
-					$milestone.find(".livestamp").livestamp(milestoneMoment);
+					secondsFromNow = getSecondsFromNow(milestoneMoment);
+					$milestone.find(".flipclock")
+							.empty()
+							.FlipClock(Math.abs(secondsFromNow), {
+								clockFace: 'DailyCounter',
+								countdown: (secondsFromNow >= 0)
+							});
+					$milestone.find(".flipclock").livestamp(milestoneMoment);
 					$milestone.find(".date").text(milestoneMoment.toLocaleString());
 				}
 			}
