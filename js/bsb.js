55c279b24dc1041d57d3ea554ef4223797759d45
offDaCharts <robqthames@gmail.com>
2014-08-13 01:36:13 -0700

    first version of next milestone counters

--- js/bsb.js
+++ js/bsb.js
@@ -4,11 +4,6 @@ $(function() {
     $fullpage.fullpage({
         afterLoad: function(anchorLink, index) {
             var tableDisplayPageNum = 2,
-                secondMilestones = {
-                    'billion': 1e9,
-                    'million': 1e6,
-                    'thousand': 1e3
-                },
                 millisecondMultiplier = 1e3,
                 bdaytime,
                 $milestone,
@@ -18,7 +13,7 @@ $(function() {
                 getSecondsFromNow = function(thisMoment) {
                     return Math.round(thisMoment.diff(moment())/millisecondMultiplier);
                 },
-                populateMilestones = function(bdaytime) {
+                populateMilestones = function(bdaytime, secondMilestones) {
                     for(milestone in secondMilestones) {
                         milestoneMoment = moment(bdaytime + secondMilestones[milestone] * millisecondMultiplier);
                         $milestone = $("#" + milestone);
@@ -32,6 +27,29 @@ $(function() {
                         $milestone.find(".livestamp").livestamp(milestoneMoment);
                         $milestone.find(".date").text(milestoneMoment.toLocaleString());
                     }
+                },
+                populateNextMilestones = function(bdaytime, quantums) {
+                    var currentSecondsAge,
+                    	ageAtnextMilestone,
+                    	nextMilestoneMoment,
+                    	$nextMilestone,
+                    	quantum;
+                    for(quantum in quantums) {
+                    	$nextMilestone = $("#next" + quantum);
+                        currentSecondsAge = Math.abs(getSecondsFromNow(bdaytime));
+                        ageAtnextMilestone = Math.ceil(currentSecondsAge/quantums[quantum]) * quantums[quantum];
+                        nextMilestoneMoment = moment(bdaytime + ageAtnextMilestone * millisecondMultiplier);
+
+                        $nextMilestone.find(".flipclock")
+                                .empty()
+                                .FlipClock(ageAtnextMilestone - currentSecondsAge, {
+                                    clockFace: 'DailyCounter',
+                                    countdown: true
+                                });
+                        $nextMilestone.find(".livestamp").livestamp(nextMilestoneMoment);
+                        $nextMilestone.find(".date").text(nextMilestoneMoment.toLocaleString());
+                    };
+                    
                 };
 
             if (index === tableDisplayPageNum) {
@@ -45,7 +63,18 @@ $(function() {
                         autoStart: true
                     });
 
-                populateMilestones(bdaytime);                
+                populateMilestones(bdaytime, {
+                    'billion': 1e9,
+                    'million': 1e6,
+                    'thousand': 1e3
+                });
+                populateNextMilestones(bdaytime, {
+                	'Year': 365*24*3600, //doesn't account for leap years...
+                    'Billion': 1e9,
+                    'Million': 1e6,
+                    'Thousand': 1e3
+                });
+
                 
             }
         }
