ce0066312f1bc0cb46372bd6e6372d24047e4b91
offDaCharts <robqthames@gmail.com>
2014-08-13 00:57:59 -0700

    resizing clocks and formatting

--- js/bsb.js
+++ js/bsb.js
@@ -1,53 +1,57 @@
 $(function() {
-	var $fullpage = $("#fullpage");
+    var $fullpage = $("#fullpage");
 
-	$fullpage.fullpage({
-		afterLoad: function(anchorLink, index) {
-			var tableDisplayPageNum = 2,
-				getSecondsFromNow = function(thisMoment) {
-					return Math.round(thisMoment.diff(moment())/millisecondMultiplier);
-				}
+    $fullpage.fullpage({
+        afterLoad: function(anchorLink, index) {
+            var tableDisplayPageNum = 2,
+                secondMilestones = {
+                    'billion': 1e9,
+                    'million': 1e6,
+                    'thousand': 1e3
+                },
+                millisecondMultiplier = 1e3,
+                bdaytime,
+                $milestone,
+                milestoneMoment,
+                secondsFromNow,
+                flipclockParameters,
+                getSecondsFromNow = function(thisMoment) {
+                    return Math.round(thisMoment.diff(moment())/millisecondMultiplier);
+                },
+                populateMilestones = function(bdaytime) {
+                    for(milestone in secondMilestones) {
+                        milestoneMoment = moment(bdaytime + secondMilestones[milestone] * millisecondMultiplier);
+                        $milestone = $("#" + milestone);
+                        secondsFromNow = getSecondsFromNow(milestoneMoment);
+                        $milestone.find(".flipclock")
+                                .empty()
+                                .FlipClock(Math.abs(secondsFromNow), {
+                                    clockFace: 'DailyCounter',
+                                    countdown: (secondsFromNow > 0)
+                                });
+                        $milestone.find(".livestamp").livestamp(milestoneMoment);
+                        $milestone.find(".date").text(milestoneMoment.toLocaleString());
+                    }
+                };
 
-			if (index === tableDisplayPageNum) {
-				var bdaytime = moment($("input[name=bdaytime]").val()),
-					secondMilestones = {
-						'billion': 1e9,
-						'million': 1e6,
-						'thousand': 1e3
-					},
-					millisecondMultiplier = 1e3,
-					$milestone,
-					milestoneMoment,
-					secondsFromNow,
-					flipclockParameters;
+            if (index === tableDisplayPageNum) {
+                bdaytime = moment($("input[name=bdaytime]").val());
 
-				$('#current .flipclock')
-					.empty()
-					.FlipClock(Math.abs(getSecondsFromNow(bdaytime)), {
-						clockFace: 'Counter',
-						countdown: (secondsFromNow <= 0),
-						autoStart: true
-					});
+                $('#current .flipclock')
+                    .empty()
+                    .FlipClock(Math.abs(getSecondsFromNow(bdaytime)), {
+                        clockFace: 'Counter',
+                        countdown: (secondsFromNow <= 0),
+                        autoStart: true
+                    });
 
+                populateMilestones(bdaytime);                
+                
+            }
+        }
+    });
 
-				for(milestone in secondMilestones) {
-					milestoneMoment = moment(bdaytime + secondMilestones[milestone] * millisecondMultiplier);
-					$milestone = $("#" + milestone);
-					secondsFromNow = getSecondsFromNow(milestoneMoment);
-					$milestone.find(".flipclock")
-							.empty()
-							.FlipClock(Math.abs(secondsFromNow), {
-								clockFace: 'DailyCounter',
-								countdown: (secondsFromNow > 0)
-							});
-					// $milestone.find(".livestamp").livestamp(milestoneMoment);
-					$milestone.find(".date").text(milestoneMoment.toLocaleString());
-				}
-			}
-		}
-	});
-
-	$("#goButton").click(function() {
-		$fullpage.fullpage.moveSectionDown();
-	});
+    $("#goButton").click(function() {
+        $fullpage.fullpage.moveSectionDown();
+    });
 });
